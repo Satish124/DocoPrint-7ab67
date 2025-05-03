21703998cbb10b0ccb99840f24d3d907ff9a5ea9
satish <71367876+Satish124@users.noreply.github.com>
2025-05-03 17:53:49 +0530

    Update page.jsx
    removed dummy content

--- app/page.jsx
+++ app/page.jsx
@@ -6,19 +6,15 @@ import { RandomQuote } from 'components/random-quote';
 import { getNetlifyContext } from 'utils';
 
 const contextExplainer = `
-The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
-([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
+lorem ipsum
 `;
 
 const preDynamicContentExplainer = `
-The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
+lorem ipsum
 `;
 
 const postDynamicContentExplainer = `
-On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
-Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
-
-And as always with dynamic content, beware of layout shifts & flicker! (here, we aren't...)
+lorem ipsum
 `;
 
 const ctx = getNetlifyContext();
@@ -26,41 +22,23 @@ const ctx = getNetlifyContext();
 export default function Page() {
     return (
         <div className="flex flex-col gap-12 sm:gap-16">
-            <section>
-                <ContextAlert className="mb-6" />
-                <h1 className="mb-4">Netlify Platform Starter - Next.js</h1>
-                <p className="mb-6 text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
-                    Read the Docs
-                </Link>
-            </section>
-            {!!ctx && (
-                <section className="flex flex-col gap-4">
-                    <Markdown content={contextExplainer} />
-                    <RuntimeContextCard />
-                </section>
-            )}
-            <section className="flex flex-col gap-4">
-                <Markdown content={preDynamicContentExplainer} />
-                <RandomQuote />
-                <Markdown content={postDynamicContentExplainer} />
-            </section>
+          
         </div>
     );
 }
 
 function RuntimeContextCard() {
-    const title = `Netlify Context: running in ${ctx} mode.`;
+    const title = `Docoprint Context: running in ${ctx} mode.`;
     if (ctx === 'dev') {
         return (
             <Card title={title}>
-                <p>Next.js will rebuild any page you navigate to, including static pages.</p>
+                <p></p>
             </Card>
         );
     } else {
         return (
             <Card title={title}>
-                <p>This page was statically-generated at build time.</p>
+                <p>Docoprint 2025</p>
             </Card>
         );
     }
